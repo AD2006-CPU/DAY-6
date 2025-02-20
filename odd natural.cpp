17a1f000b1e782b81b62f065ead3e4f039d6fdfc
AD2006-CPU <anushkadey424@gmail.com>
2025-02-20 11:05:00 +0530

    Add files via upload

---
+++ odd natural.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ 
+ int i,n=0;
+ for(i=1;i<=20;i+=2)
+{
+ n=n+i;
+}
+{
+ printf("%d",n);
+}
+return 0;
+}
---
+++ sum of 10.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int sum=0;
+	for(int i=1;i<=10;i++)
+	sum+=i*i;
+	{
+	printf("SUM OF SQUARES OF FIRST 10 NATURAL NUMBERS:%d\n",sum);
+	}
+	return 0;
+}
